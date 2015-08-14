e5c427e8175963fa569ab75d7d689d9a1117d328
Aurelie Ladier <ladier@phimeca.com>
2015-08-14 15:39:56 +0000

    Add files for the Gauge test

---
+++ lib/test/t_Gauge_std.cxx
@@ -0,0 +1,37 @@
+//                                               -*- C++ -*-
+
+#include "OT.hxx"
+#include "OTtestcode.hxx"
+#include "OTStudy.hxx"
+#include "YACSPhysicalModel.hxx"
+
+
+using namespace OT;
+using namespace OT::Test;
+using namespace OTGUI;
+
+int main(int argc, char *argv[])
+{
+  TESTPREAMBLE;
+  OStream fullprint(std::cout);
+
+  try
+  {
+    OTStudy myStudy("myStudy");
+
+    YACSPhysicalModel myPhysicalModel("myPhysicalModel", "example.xml");
+    myPhysicalModel.loadDataWithYACS();
+    myStudy.addPhysicalModel(myPhysicalModel);
+
+    // Comparaison
+//     assert_almost_equal(outputSample, resultSample, 1e-16);
+  }
+  catch (TestFailed & ex)
+  {
+    std::cerr << ex << std::endl;
+    return ExitCode::Error;
+  }
+
+
+  return ExitCode::Success;
+}
