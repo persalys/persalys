55344ee17cac167a85e030635f15e7bc547d2a40
Aurelie Ladier <ladier@phimeca.com>
2016-01-12 09:04:39 +0000

    Clean source code

--- lib/src/view/OTguiMenuBar.cxx
+++ lib/src/view/OTguiMenuBar.cxx
@@ -14,23 +14,23 @@ OTguiMenuBar::OTguiMenuBar()
 void OTguiMenuBar::buildActions()
 {
   QMenu * menu = new QMenu(tr("&Menu"));
-  QList<QAction* > actions;
 
   QAction * action = new QAction(QIcon(":/images/document-new.png"), tr("&New OTStudy"), this);
   action->setStatusTip(tr("Create a new OTStudy"));
   connect(action, SIGNAL(triggered()), this, SIGNAL(createNewOTStudy()));
-  actions.append(action);
+  menu->addAction(action);
 
   action = new QAction(QIcon(":/images/document-import.png"), tr("&Load OTStudy"), this);
   action->setStatusTip(tr("Load an OTStudy"));
   connect(action, SIGNAL(triggered()), this, SIGNAL(loadOTStudy()));
-  actions.append(action);
+  menu->addAction(action);
+
+  addSeparator();
 
   action = new QAction(QIcon(":/images/window-close.png"), tr("E&xit"), this);
   connect(action, SIGNAL(triggered()), this, SIGNAL(closeWindow()));
-  actions.append(action);
+  menu->addAction(action);
 
-  menu->addActions(actions);
   addMenu(menu);
 }
 }
