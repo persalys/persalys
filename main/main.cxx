339587989b34738c5b0dd9b5b59ca5c6a61e229d
Aurelie Ladier <ladier@phimeca.com>
2016-02-04 12:46:19 +0000

    Resize main window.

--- main/main.cxx
+++ main/main.cxx
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     MainWindow window;
 
-    window.resize(640, 480);
+    window.resize(1024, 768);
     window.show();
     window.launchInitialMessageBox();
 
