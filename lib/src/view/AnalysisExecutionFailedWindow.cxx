fa66f7bb6c669494bb7d611856bdde8ff4fcd0bb
Aurelie Ladier <ladier@phimeca.com>
2016-02-25 17:37:12 +0000

    Add AnalysisExecutionFailedWindow class.

---
+++ lib/src/view/AnalysisExecutionFailedWindow.cxx
@@ -0,0 +1,57 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief Base class for otgui error window
+ *
+ *  Copyright 2015-2016 EDF
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "otgui/AnalysisExecutionFailedWindow.hxx"
+
+#include <QVBoxLayout>
+
+using namespace OT;
+
+namespace OTGUI {
+
+AnalysisExecutionFailedWindow::AnalysisExecutionFailedWindow(AnalysisItem * item, const QString & errorMessage)
+  : OTguiSubWindow(item)
+{
+  buildInterface();
+  setErrorMessage(errorMessage);
+}
+
+
+void AnalysisExecutionFailedWindow::buildInterface()
+{
+  setWindowTitle("Error window");
+
+  QWidget * mainWidget = new QWidget;
+  QVBoxLayout * layout = new QVBoxLayout(mainWidget);
+
+  QString message(tr("No results are available. An error has occured during the execution of the analysis:\n"));
+  QLabel * label = new QLabel(message);
+  label->setWordWrap(true);
+  layout->addWidget(label);
+
+  errorMessageLabel_ = new QLabel;
+  errorMessageLabel_->setWordWrap(true);
+  layout->addWidget(errorMessageLabel_);
+
+  layout->addStretch();
+
+  setWidget(mainWidget);
+}
+}
---
+++ lib/src/view/otgui/AnalysisExecutionFailedWindow.hxx
@@ -0,0 +1,37 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief Base class for otgui error window
+ *
+ *  Copyright 2015-2016 EDF
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OTGUI_ANALYSISEXECUTIONFAILEDWINDOW_HXX
+#define OTGUI_ANALYSISEXECUTIONFAILEDWINDOW_HXX
+
+#include "otgui/OTguiSubWindow.hxx"
+#include "otgui/AnalysisItem.hxx"
+
+namespace OTGUI {
+class AnalysisExecutionFailedWindow : public OTguiSubWindow
+{
+public:
+  AnalysisExecutionFailedWindow(AnalysisItem * item, const QString & errorMessage);
+
+protected:
+  void buildInterface();
+};
+}
+#endif
