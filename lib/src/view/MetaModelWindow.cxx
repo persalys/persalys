1f6083ac8670200ef2de73332f6c611e0f04f27d
Aurélie Ladier <ladier@phimeca.com>
2016-09-15 14:24:27 +0200

    Add MetaModelWindow class

---
+++ lib/src/view/MetaModelWindow.cxx
@@ -0,0 +1,35 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief QMdiSubWindow to define a physical model
+ *
+ *  Copyright 2015-2016 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "otgui/MetaModelWindow.hxx"
+
+#include "otgui/PhysicalModelWindowWidget.hxx"
+
+namespace OTGUI {
+
+MetaModelWindow::MetaModelWindow(PhysicalModelItem * item)
+  : OTguiSubWindow(item)
+  , physicalModel_(item->getPhysicalModel())
+{
+  PhysicalModelWindowWidget * widget = new PhysicalModelWindowWidget(item);
+
+  setWidget(widget);
+}
+}
