5fdddd10bd9da43d4fcaf28a78c0c55386ed43c8
Aurélie Ladier <ladier@phimeca.com>
2016-12-16 16:27:29 +0100

    Add test KrigingAnalysis

---
+++ python/test/t_KrigingAnalysis_std.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import openturns as ot
+import openturns.testing
+import otguibase
+
+ot.RandomGenerator.SetSeed(0)
+ot.TBB_Disable()
+
+myStudy = otguibase.OTStudy('myStudy')
+
+## Model
+xi1 = otguibase.Input('xi1', 0., '', ot.Uniform(0., 10.))
+xi2 = otguibase.Input('xi2', 0., '', ot.Uniform(0., 10.))
+y00 = otguibase.Output('fake_y0', 0, '', 'xi1')
+y00.setIsSelected(False)
+formula = "cos(0.5*xi1) + sin(xi2)"
+y0 = otguibase.Output('y0', 0., '', formula)
+
+model = otguibase.AnalyticalPhysicalModel('model', [xi1, xi2], [y00, y0])
+myStudy.add(model)
+
+## Design of Experiment ##
+aDesign = otguibase.DesignOfExperimentImplementation('design', model)
+inputSample = ot.LHSExperiment(model.getComposedDistribution(), 10).generate()
+aDesign.setInputSample(inputSample)
+myStudy.add(aDesign)
+
+aDesign.run()
+
+## Kriging ##
+analysis = otguibase.KrigingAnalysis('kriging_0', aDesign)
+analysis.setBasis(ot.LinearBasisFactory(2).build())
+analysis.setCovarianceModel(ot.MaternModel(2))
+myStudy.add(analysis)
+print(analysis)
+
+analysis.run()
+
+metaModel = analysis.getResult().getResultForVariable('y0').getMetaModel()
+openturns.testing.assert_almost_equal(aDesign.getOutputSample(), metaModel(inputSample), 3.0e-5, 3.0e-5)
+
+
+## Design of Experiment ##
+model.addOutput(otguibase.Output('y1', 0., '', formula))
+model.addOutput(otguibase.Output('y2', 0., '', "(cos(0.5*xi1) + sin(xi2))*2"))
+aDesign.run()
+
+## Kriging ##
+analysis2 = otguibase.KrigingAnalysis('kriging_1', aDesign)
+analysis2.setLeaveOneOutValidation(True)
+analysis2.setInterestVariables(['y1', 'y2'])
+myStudy.add(analysis2)
+print(analysis2)
+
+analysis2.run()
+
+metaModel2 = analysis2.getResult().getResultForVariable('y1').getMetaModel()
+openturns.testing.assert_almost_equal(aDesign.getOutputSample().getMarginal(1), metaModel2(inputSample), 3.0e-5, 3.0e-5)
+openturns.testing.assert_almost_equal(analysis2.getResult().getQ2LeaveOneOut(), [0.832278,0.832264], 1e-3, 1e-3)
+
+# script
+script = myStudy.getPythonScript()
+print(script)
+exec(script)
