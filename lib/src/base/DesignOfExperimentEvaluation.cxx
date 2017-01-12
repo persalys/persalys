2388de2b559566ad4f7a3cbbf78c7063fcefdf68
Aurélie Ladier <ladier@phimeca.com>
2017-01-12 11:04:09 +0100

    Add analysis class DesignOfExperimentEvaluation

---
+++ lib/src/base/DesignOfExperimentEvaluation.cxx
@@ -0,0 +1,72 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief Evaluates the design of experiment
+ *
+ *  Copyright 2015-2016 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "otgui/DesignOfExperimentEvaluation.hxx"
+
+using namespace OT;
+
+namespace OTGUI {
+
+/* Constructor with parameters */
+DesignOfExperimentEvaluation::DesignOfExperimentEvaluation(const DesignOfExperiment& design)
+  : DesignOfExperimentAnalysis("evaluation", design)
+  , Observer()
+{
+  if (!design.hasPhysicalModel())
+    throw InvalidArgumentException(HERE) << "The design of experiment must be built from a physical model";
+
+  AnalysisImplementation::setInterestVariables(design.getPhysicalModel().getSelectedOutputsNames());
+}
+
+
+/* Virtual constructor */
+DesignOfExperimentEvaluation* DesignOfExperimentEvaluation::clone() const
+{
+  return new DesignOfExperimentEvaluation(*this);
+}
+
+
+void DesignOfExperimentEvaluation::run()
+{
+  getDesignOfExperiment().getImplementation()->addObserver(this);
+  getDesignOfExperiment().run();
+  getDesignOfExperiment().getImplementation()->removeObserver(this);
+}
+
+
+void DesignOfExperimentEvaluation::stop()
+{
+  getDesignOfExperiment().stop();
+}
+
+
+void DesignOfExperimentEvaluation::update(Observable* source, const String& message)
+{
+  if (message == "analysisFinished")
+  {
+    notify("analysisFinished");
+  }
+  else if (message == "analysisBadlyFinished")
+  {
+    errorMessage_ = getDesignOfExperiment().getErrorMessage();
+    notify("analysisBadlyFinished");
+  }
+}
+}
---
+++ lib/src/base/otgui/DesignOfExperimentEvaluation.hxx
@@ -0,0 +1,44 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief Evaluates the design of experiment
+ *
+ *  Copyright 2015-2016 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OTGUI_DESIGNOFEXPERIMENTEVALUATION_HXX
+#define OTGUI_DESIGNOFEXPERIMENTEVALUATION_HXX
+
+#include "DesignOfExperimentAnalysis.hxx"
+
+namespace OTGUI {
+class OTGUI_API DesignOfExperimentEvaluation : public DesignOfExperimentAnalysis, public Observer
+{
+  /** Fake analysis to be enable to launch the evaluation of the DOE in a separate thread from StudyTreeView */
+
+public:
+  /** Constructor with parameters */
+  DesignOfExperimentEvaluation(const DesignOfExperiment& design);
+
+  /** Virtual constructor */
+  virtual DesignOfExperimentEvaluation * clone() const;
+
+  virtual void run();
+  virtual void stop();
+
+  virtual void update(Observable * source, const OT::String & message);
+};
+}
+#endif
