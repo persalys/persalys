1164f8bf0f843df6a67eb4208a180210b046bb54
Aurélie Ladier <ladier@phimeca.com>
2017-01-12 14:16:07 +0100

    Add classes Controller and ControllerWidget

---
+++ lib/src/view/otgui/Controller.hxx
@@ -0,0 +1,88 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief QObject to run an analysis in a separate thread
+ *
+ *  Copyright 2015-2016 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OTGUI_CONTROLLER_HXX
+#define OTGUI_CONTROLLER_HXX
+
+#include "otgui/Analysis.hxx"
+
+#include <QThread>
+#include <QMetaType>
+
+namespace OTGUI {
+class Worker : public QObject
+{
+    Q_OBJECT
+
+public slots:
+  void process(Analysis analysis)
+  {
+    analysis.run();
+    emit processFinished();
+  }
+
+signals:
+  void processFinished();
+};
+
+class Controller : public QObject
+{
+  Q_OBJECT
+
+public:
+  Controller()
+  {
+    qRegisterMetaType<OTGUI::PhysicalModel>("PhysicalModel");
+    qRegisterMetaType<OTGUI::Analysis>("Analysis");
+
+    // new thread
+    QThread * workerThread = new QThread;
+    connect(workerThread, SIGNAL(finished()), workerThread, SLOT(deleteLater()));
+
+    // worker
+    Worker * worker = new Worker;
+    worker->moveToThread(workerThread);
+
+    // connections
+    connect(this, SIGNAL(launchAnalysisRequested(Analysis)), worker, SLOT(process(Analysis)));
+    connect(worker, SIGNAL(processFinished()), this, SIGNAL(analysisFinished()));
+    connect(worker, SIGNAL(processFinished()), workerThread, SLOT(quit()));
+    connect(worker, SIGNAL(processFinished()), worker, SLOT(deleteLater()));
+    connect(worker, SIGNAL(processFinished()), this, SLOT(deleteLater()));
+
+    // start thread
+    workerThread->start();
+  }
+
+public slots:
+  void launchAnalysis(Analysis analysis)
+  {
+    emit launchAnalysisRequested(analysis);
+
+    // Warning : NOT create worker as attribut and then write here worker.process():
+    // otherwise the run method WON'T be launch in a separate thread !
+  };
+
+signals:
+  void launchAnalysisRequested(Analysis analysis);
+  void analysisFinished();
+};
+}
+#endif
---
+++ lib/src/view/otgui/ControllerWidget.hxx
@@ -0,0 +1,100 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief QWidget with a progressBar which allows to stop analysis
+ *
+ *  Copyright 2015-2016 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OTGUI_CONTROLLERWIDGET_HXX
+#define OTGUI_CONTROLLERWIDGET_HXX
+
+#include <QVBoxLayout>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QDateTime>
+#include <QLabel>
+#include <QScrollArea>
+
+namespace OTGUI {
+class ControllerWidget : public QWidget
+{
+  Q_OBJECT
+
+public:
+  ControllerWidget(const QString& analysisName, QWidget* parent = 0)
+  : QWidget(parent)
+  {
+    QVBoxLayout * widgetLayout = new QVBoxLayout(this);
+
+    QScrollArea * scrollArea = new QScrollArea;
+    scrollArea->setWidgetResizable(true);
+    QFrame * frame = new QFrame;
+    QVBoxLayout * mainLayout = new QVBoxLayout(frame);
+
+    // analysis name
+    QString dateFormat("d MMMM yyyy h:mm:ss");
+    QLabel * aLabel = new QLabel(tr("Run the analysis '%1'.\nLaunch time: %2.").arg(analysisName).arg(QDateTime::currentDateTime().toString(dateFormat)));
+    aLabel->setWordWrap(true);
+    mainLayout->addWidget(aLabel);
+
+    // progress bar
+    progressBar_ = new QProgressBar;
+    //To start indefinite/busy progress bar
+    progressBar_->setRange(0, 0);
+    progressBar_->setValue(10);
+    mainLayout->addWidget(progressBar_);
+
+    // information message
+    messageLabel_ = new QLabel;
+    messageLabel_->setWordWrap(true);
+    mainLayout->addWidget(messageLabel_);
+
+    // stop button
+    QHBoxLayout * hLayout = new QHBoxLayout;
+    hLayout->addStretch();
+    QPushButton * stopButton = new QPushButton(tr("Stop"));
+    connect(stopButton, SIGNAL(clicked(bool)), this, SIGNAL(stopRunningRequested()));
+    hLayout->addWidget(stopButton);
+    mainLayout->addLayout(hLayout);
+
+    mainLayout->addStretch();
+
+    scrollArea->setWidget(frame);
+    widgetLayout->addWidget(scrollArea);
+  }
+
+public slots:
+  void changeProgressBarValue(int value)
+  {
+    progressBar_->setRange(0, 100);
+    progressBar_->setValue(value);
+  }
+
+
+  void setMessage(const QString message)
+  {
+    messageLabel_->setText(message);
+  }
+
+signals:
+  void stopRunningRequested();
+
+private:
+  QLabel * messageLabel_;
+  QProgressBar * progressBar_;
+};
+}
+#endif
