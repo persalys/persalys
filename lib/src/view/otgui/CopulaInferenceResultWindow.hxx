4bc2eeaebcef873ede77c3cd05ba78e323f57fb5
Aurélie Ladier <ladier@phimeca.com>
2017-02-01 16:42:30 +0100

    Add widgets for dependency inference

---
+++ lib/src/view/otgui/CopulaInferenceResultWindow.hxx
@@ -0,0 +1,50 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief QMdiSubWindow for the results of InferenceAnalysis
+ *
+ *  Copyright 2015-2016 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OTGUI_COPULAINFERENCERESULTWINDOW_HXX
+#define OTGUI_COPULAINFERENCERESULTWINDOW_HXX
+
+#include "otgui/ResultWindow.hxx"
+#include "otgui/CopulaInferenceResult.hxx"
+#include "otgui/GraphConfigurationWidget.hxx"
+#include "otgui/CustomStandardItemModel.hxx"
+#include "otgui/CopulaInferenceResultWidget.hxx"
+
+namespace OTGUI {
+class OTGUI_API CopulaInferenceResultWindow : public ResultWindow
+{
+  Q_OBJECT
+
+public:
+  CopulaInferenceResultWindow(AnalysisItem * item);
+
+protected:
+  void buildInterface();
+
+public slots:
+  void showHideGraphConfigurationWidget(Qt::WindowStates, Qt::WindowStates);
+signals:
+  void windowActivated();
+
+private:
+  CopulaInferenceResult result_;
+};
+}
+#endif
---
+++ lib/src/view/otgui/CopulaParametersTabWidget.hxx
@@ -0,0 +1,55 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief QTabWidget for the parameters and PDF/CDF copula
+ *
+ *  Copyright 2015-2016 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OTGUI_COPULAPARAMETERSTABWIDGET_HXX
+#define OTGUI_COPULAPARAMETERSTABWIDGET_HXX
+
+#include "otgui/GraphConfigurationWidget.hxx"
+
+#include <QTabWidget>
+
+namespace OTGUI {
+class OTGUI_API CopulaParametersTabWidget : public QTabWidget
+{
+  Q_OBJECT
+
+public:
+  CopulaParametersTabWidget(const OT::Distribution& distribution,
+                            const OT::NumericalSample& sample,
+                            QWidget* parent = 0);
+
+protected:
+  void buildInterface();
+
+public slots:
+  void stateChanged();  
+  void showHideGraphConfigurationWidget(int index);
+
+signals:
+  void graphWindowActivated(QWidget*);
+  void graphWindowDeactivated();
+
+private:
+  OT::Distribution distribution_;
+  OT::NumericalSample sample_;
+  GraphConfigurationWidget * pdf_cdfPlotGraphConfigWidget_;
+};
+}
+#endif
