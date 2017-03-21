fe5b077b785d21e90b36339b11ed54575a8ed555
Aurélie Ladier <ladier@phimeca.com>
2017-03-21 10:40:59 +0100

    Add deterministic inputs in tests

--- python/test/t_KrigingAnalysis_std.py
+++ python/test/t_KrigingAnalysis_std.py
@@ -13,18 +13,21 @@ myStudy = otguibase.OTStudy('myStudy')
 ## Model
 xi1 = otguibase.Input('xi1', ot.Uniform(0., 10.))
 xi2 = otguibase.Input('xi2', ot.Uniform(0., 10.))
+xi3 = otguibase.Input('xi3', 0.5)
 y00 = otguibase.Output('fake_y0')
 y00.setIsSelected(False)
 y0 = otguibase.Output('y0')
 
 formula_y00 = "xi1"
 formula_y0 = "cos(0.5*xi1) + sin(xi2)"
-model = otguibase.SymbolicPhysicalModel('model', [xi1, xi2], [y00, y0], [formula_y00, formula_y0])
+model = otguibase.SymbolicPhysicalModel('model', [xi1, xi2, xi3], [y00, y0], [formula_y00, formula_y0])
 myStudy.add(model)
 
 ## Design of Experiment ##
 aDesign = otguibase.DesignOfExperimentImplementation('design', model)
-inputSample = ot.LHSExperiment(model.getComposedDistribution(), 10).generate()
+validationInputSample = ot.LHSExperiment(model.getComposedDistribution(), 10).generate()
+inputSample = ot.NumericalSample(validationInputSample)
+inputSample.stack(ot.NumericalSample(10, [0.5]))
 aDesign.setInputSample(inputSample)
 myStudy.add(aDesign)
 
@@ -39,29 +42,33 @@ print(analysis)
 
 analysis.run()
 
+result = analysis.getResult()
+print("result=", result)
+
 metaModel = analysis.getResult().getResultForVariable('y0').getMetaModel()
-openturns.testing.assert_almost_equal(aDesign.getOutputSample(), metaModel(inputSample), 3.0e-5, 3.0e-5)
+openturns.testing.assert_almost_equal(aDesign.getOutputSample(), metaModel(validationInputSample), 3.0e-5, 3.0e-5)
 
 
 ## Design of Experiment ##
 model.addOutput(otguibase.Output('y1'))
-model.setFormula('y1', formula_y0)
-model.addOutput(otguibase.Output('y2'))
-model.setFormula('y2', '(cos(0.5*xi1) + sin(xi2))*2')
+model.setFormula('y1', formula_y0 + ' + xi3')
 aDesign.run()
 
 ## Kriging ##
 analysis2 = otguibase.KrigingAnalysis('kriging_1', aDesign)
 analysis2.setLeaveOneOutValidation(True)
-analysis2.setInterestVariables(['y1', 'y2'])
+analysis2.setInterestVariables(['y1', 'y0'])
 myStudy.add(analysis2)
 print(analysis2)
 
 analysis2.run()
 
-metaModel2 = analysis2.getResult().getResultForVariable('y1').getMetaModel()
-openturns.testing.assert_almost_equal(aDesign.getOutputSample().getMarginal(1), metaModel2(inputSample), 3.0e-5, 3.0e-5)
-openturns.testing.assert_almost_equal(analysis2.getResult().getQ2LeaveOneOut(), [0.832278,0.832264], 1e-3, 1e-3)
+result2 = analysis2.getResult()
+print("result=", result2)
+
+metaModel2 = result2.getResultForVariable('y1').getMetaModel()
+openturns.testing.assert_almost_equal(aDesign.getOutputSample().getMarginal(1), metaModel2(validationInputSample), 3.0e-5, 3.0e-5)
+openturns.testing.assert_almost_equal(result2.getQ2LeaveOneOut(), [0.832447,0.832278], 1e-3, 1e-3)
 
 # script
 script = myStudy.getPythonScript()
