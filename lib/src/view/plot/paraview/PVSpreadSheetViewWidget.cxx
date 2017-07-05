005ea11d6b5358f9c4ba9c0b79e7ee42fb015eb7
ladier <ladier@phimeca.com>
2017-07-05 10:38:32 +0200

    Add PVSpreadSheetViewWidget

---
+++ lib/src/view/plot/paraview/PVSpreadSheetViewWidget.cxx
@@ -0,0 +1,46 @@
+#include "otgui/PVSpreadSheetViewWidget.hxx"
+
+#include <pqSaveDataReaction.h>
+#include <pqActiveObjects.h>
+
+#include <QMenu>
+
+namespace OTGUI {
+const char PVSpreadSheetViewWidget::PV_VIEW_TYPE[] = "SpreadSheetView";
+
+const char PVSpreadSheetViewWidget::PV_REPRESENTATION_TYPE[] = "SpreadSheetRepresentation";
+
+PVSpreadSheetViewWidget::PVSpreadSheetViewWidget(QWidget *parent, PVServerManagerInterface *smb)
+  : PVViewWidget(parent, smb, PV_VIEW_TYPE)
+{
+  // context menu
+  setContextMenuPolicy(Qt::CustomContextMenu);
+  connect(this, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(contextMenu(QPoint)));
+}
+
+
+PVSpreadSheetViewWidget::~PVSpreadSheetViewWidget()
+{
+}
+
+
+// show the context menu when right clicking
+void PVSpreadSheetViewWidget::contextMenu(const QPoint& pos)
+{
+  // action
+  QAction * exportDataAction = new QAction(QIcon(":/images/document-export-table.png"), tr("Export"), this);
+  connect(exportDataAction, SIGNAL(triggered(bool)), this, SLOT(exportData()));
+
+  // menu
+  QMenu * contextMenu(new QMenu(this));
+  contextMenu->addAction(exportDataAction);
+  contextMenu->popup(mapToGlobal(pos));
+}
+
+
+void PVSpreadSheetViewWidget::exportData()
+{
+  pqActiveObjects::instance().setActiveView(getView());
+  pqSaveDataReaction::saveActiveData();
+}
+}
---
+++ lib/src/view/plot/paraview/otgui/PVSpreadSheetViewWidget.hxx
@@ -0,0 +1,22 @@
+#ifndef __PV_SPREADSHEETVIEW_WIDGET_HXX__
+#define __PV_SPREADSHEETVIEW_WIDGET_HXX__
+
+#include "PVViewWidget.hxx"
+
+namespace OTGUI {
+class OTGUI_API PVSpreadSheetViewWidget : public PVViewWidget
+{
+  Q_OBJECT
+public:
+  PVSpreadSheetViewWidget(QWidget *parent, PVServerManagerInterface *smb);
+  ~PVSpreadSheetViewWidget();
+  virtual const char *getRepresentationName() const { return PV_REPRESENTATION_TYPE; }
+public slots:
+  void contextMenu(const QPoint & pos);
+  void exportData();
+public:
+  static const char PV_VIEW_TYPE[];
+  static const char PV_REPRESENTATION_TYPE[];
+};
+}
+#endif
