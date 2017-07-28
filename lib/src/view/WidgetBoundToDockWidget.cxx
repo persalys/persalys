a4474cd8ea9e24eab930c93e550fb98d17abea47
ladier <ladier@phimeca.com>
2017-07-28 17:39:32 +0200

    Add WidgetBoundToDockWidget

---
+++ lib/src/view/WidgetBoundToDockWidget.cxx
@@ -0,0 +1,127 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief QWidget bound to a dock widget
+ *
+ *  Copyright 2015-2017 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "otgui/WidgetBoundToDockWidget.hxx"
+
+#include "otgui/OTguiException.hxx"
+
+using namespace OT;
+
+namespace OTGUI {
+
+WidgetBoundToDockWidget::WidgetBoundToDockWidget(QWidget* parent)
+  : QWidget(parent)
+  , dockWidgetAlreadyShown_(false)
+{
+  MainWindow * mainWindow = findMainWindowInHierachy();
+  if (!mainWindow)
+    throw InvalidArgumentException(HERE) << "Internal error : No main window found !";
+
+  connect(this, SIGNAL(showDockWidgetRequested(QWidget*)), mainWindow, SLOT(showGraphSettingDockWidget(QWidget*)));
+  connect(this, SIGNAL(hideDockWidgetRequested(QWidget*)), mainWindow, SLOT(hideGraphSettingDockWidget(QWidget*)));
+
+  parentSubWindow_ = findSubWindowInHierachy();
+  if (!parentSubWindow_)
+    throw InvalidArgumentException(HERE) << "Internal error : No mdi sub window found !";
+  connect(parentSubWindow_, SIGNAL(windowStateChanged(Qt::WindowStates, Qt::WindowStates)), this, SLOT(updateDockWidgetVisibility(Qt::WindowStates, Qt::WindowStates)));
+}
+
+
+MainWindow * WidgetBoundToDockWidget::findMainWindowInHierachy()
+{
+  QObject * curPar(parent());
+  while (curPar)
+  {
+    MainWindow * isMW(qobject_cast<MainWindow *>(curPar));
+    if (isMW)
+      return isMW;
+    curPar = curPar->parent();
+  }
+  return 0;
+}
+
+
+QMdiSubWindow * WidgetBoundToDockWidget::findSubWindowInHierachy()
+{
+  QObject * curPar(parent());
+  while (curPar)
+  {
+    QMdiSubWindow * isMW(dynamic_cast<QMdiSubWindow *>(curPar));
+    if (isMW)
+      return isMW;
+    curPar = curPar->parent();
+  }
+  return 0;
+}
+
+
+void WidgetBoundToDockWidget::setDockWidget(QWidget* dockWidget)
+{
+  dockWidget_ = dockWidget;
+}
+
+
+void WidgetBoundToDockWidget::showEvent(QShowEvent*)
+{
+  showDockWidget();
+}
+
+
+void WidgetBoundToDockWidget::hideEvent(QHideEvent*)
+{
+  hideDockWidget();
+}
+
+
+void WidgetBoundToDockWidget::showDockWidget()
+{
+  if (!dockWidget_)
+    return;
+
+  if (parentSubWindow_->windowState() == Qt::WindowActive || parentSubWindow_->windowState() == (Qt::WindowActive|Qt::WindowMaximized))
+  {
+    dockWidgetAlreadyShown_ = true;
+    emit showDockWidgetRequested(dockWidget_);
+  }
+}
+
+
+void WidgetBoundToDockWidget::hideDockWidget()
+{
+  if (!dockWidget_ || !dockWidgetAlreadyShown_)
+    return;
+
+  dockWidgetAlreadyShown_ = false;
+  emit hideDockWidgetRequested(dockWidget_);
+}
+
+
+void WidgetBoundToDockWidget::updateDockWidgetVisibility(Qt::WindowStates /*oldState*/, Qt::WindowStates newState)
+{
+  if ((newState == Qt::WindowActive || newState == (Qt::WindowActive|Qt::WindowMaximized)) && !dockWidgetAlreadyShown_ && isVisible())
+  {
+    showDockWidget();
+  }
+  else if (newState == Qt::WindowNoState && dockWidgetAlreadyShown_)
+  {
+    hideDockWidget();
+  }
+}
+}
---
+++ lib/src/view/otgui/WidgetBoundToDockWidget.hxx
@@ -0,0 +1,61 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief QWidget bound to a dock widget
+ *
+ *  Copyright 2015-2017 EDF-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OTGUI_WIDGETBOUNDTODOCKWIDGET_HXX
+#define OTGUI_WIDGETBOUNDTODOCKWIDGET_HXX
+
+#include "otgui/MainWindow.hxx"
+#include "otgui/OTGuiprivate.hxx"
+
+#include <QMdiSubWindow>
+
+namespace OTGUI {
+class OTGUI_API WidgetBoundToDockWidget : public QWidget 
+{ 
+  Q_OBJECT
+
+public:
+  WidgetBoundToDockWidget(QWidget* parent=0);
+
+  MainWindow * findMainWindowInHierachy();
+  QMdiSubWindow * findSubWindowInHierachy();
+
+  void setDockWidget(QWidget*);
+
+  virtual void showEvent(QShowEvent*);
+  virtual void hideEvent(QHideEvent*);
+
+protected:
+  void showDockWidget();
+  void hideDockWidget();
+
+public slots:
+  void updateDockWidgetVisibility(Qt::WindowStates, Qt::WindowStates);
+signals:
+  void showDockWidgetRequested(QWidget*);
+  void hideDockWidgetRequested(QWidget*);
+
+private:
+ QWidget * dockWidget_;
+ bool dockWidgetAlreadyShown_;
+ QMdiSubWindow * parentSubWindow_;
+};
+}
+#endif
