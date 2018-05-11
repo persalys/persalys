0f796b5b8cb7562c4f94b9c85e2c9729aebf2746
Aurélie Ladier <ladier@phimeca.com>
2018-05-11 16:16:30 +0200

    Add About OTGui in Help menu

--- lib/src/view/otgui/OTguiMenuBar.hxx
+++ lib/src/view/otgui/OTguiMenuBar.hxx
@@ -43,6 +43,7 @@ public slots:
   void updateRecentFilesList(const QString & fileName);
   void updateRecentFilesActionsList();
   void clearRecentFilesActions();
+  void openAboutDialog();
 signals:
   void openOTStudy(QString);
   void showHidePythonConsole(bool);
