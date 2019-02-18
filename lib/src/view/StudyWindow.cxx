5247a6f7b141ac255bbf183f4eeef375eea9360e
Aurélie Ladier <ladier@phimeca.com>
2019-02-18 17:55:41 +0100

    Proposition StudyWindow layout improvement

--- lib/src/view/StudyWindow.cxx
+++ lib/src/view/StudyWindow.cxx
@@ -22,6 +22,7 @@
 #include "otgui/DiagramPushButton.hxx"
 
 #include <QVBoxLayout>
+#include <QScrollArea>
 
 namespace OTGUI
 {
@@ -36,60 +37,78 @@ StudyWindow::StudyWindow(StudyItem * item, QWidget * parent)
 
 void StudyWindow::buildInterface()
 {
-  QVBoxLayout * mainLayout = new QVBoxLayout(this);
-
-  mainLayout->addWidget(new TitleLabel(tr("Model creation")));
-
-  // spacer
-  mainLayout->addSpacing(10);
+  QVBoxLayout * subWinowLayout = new QVBoxLayout(this);
+  subWinowLayout->addWidget(new TitleLabel(tr("Model creation")));
 
   // buttons
-  QGridLayout * layout = new QGridLayout;
+  QScrollArea * scrollArea = new QScrollArea;
+  scrollArea->setWidgetResizable(true);
+  subWinowLayout->addWidget(scrollArea);
 
-  // left side
+  QWidget * mainWidget = new QWidget;
+  QGridLayout * layout = new QGridLayout(mainWidget);
+  scrollArea->setWidget(mainWidget);
 
+  int row = 0;
+
+  // row - titles
+  QLabel * textEdit = new QLabel;
+  textEdit->setText(tr("Available models"));
+  textEdit->setStyleSheet("QLabel {font: bold;}");
+  layout->addWidget(textEdit, row, 0);
+
+  textEdit = new QLabel;
+  textEdit->setText(tr("Available functionalities"));
+  textEdit->setStyleSheet("QLabel {font: bold;}");
+  layout->addWidget(textEdit, row, 1);
+
+  // row - HLine
+  QHBoxLayout * hLayout = new QHBoxLayout;
+  QFrame * hLineFrame = new QFrame;
+  hLineFrame->setFrameShape(QFrame::HLine);
+  hLayout->addWidget(hLineFrame, 1);
+
+  QLabel * title = new QLabel;
+  title->setText(tr("Vector function"));
+  title->setStyleSheet("border: 1px solid black");
+  hLayout->addWidget(title);
+
+  hLineFrame = new QFrame;
+  hLineFrame->setFrameShape(QFrame::HLine);
+  hLayout->addWidget(hLineFrame, 1);
+
+  layout->addLayout(hLayout, ++row, 0, 1, 2);
+
+  // row - buttons
+  int nbModels = 2;
   QPushButton * button = new DiagramPushButton(tr("Symbolic model"));
   button->setStatusTip(tr("Create a physical model with outputs defined by analytical formulae"));
   connect(button, SIGNAL(clicked()), studyItem_, SLOT(createSymbolicModel()));
-  layout->addWidget(button, 0, 0);
-
-  button = new DiagramPushButton(tr("Field Symbolic model"));
-  button->setStatusTip(tr("Create a physical model with outputs defined by analytical formulae"));
-  connect(button, SIGNAL(clicked()), studyItem_, SLOT(createSymbolicFieldModel()));
-  layout->addWidget(button, 0, 1);
+  layout->addWidget(button, ++row, 0, Qt::AlignTop);
 
   button = new DiagramPushButton(tr("Python model"));
   button->setStatusTip(tr("Create a physical model defined with a Python script"));
   connect(button, SIGNAL(clicked()), studyItem_, SLOT(createPythonModel()));
-  layout->addWidget(button, 1, 0);
+  layout->addWidget(button, ++row, 0, Qt::AlignTop);
 
-  button = new DiagramPushButton(tr("Field Python model"));
-  button->setStatusTip(tr("Create a physical model defined with a Python script"));
-  connect(button, SIGNAL(clicked()), studyItem_, SLOT(createPythonFieldModel()));
-  layout->addWidget(button, 1, 1);
-
-  int row = 2;
 #ifdef OTGUI_HAVE_YACS
   button = new DiagramPushButton(tr("YACS model"));
   button->setStatusTip(tr("Create a physical model defined with an YACS file"));
   connect(button, SIGNAL(clicked()), studyItem_, SLOT(createYACSModel()));
-  layout->addWidget(button, row, 0);
-  ++row;
+  layout->addWidget(button, ++row, 0, Qt::AlignTop);
+  ++nbModels;
 #endif
 
 #ifdef OTGUI_HAVE_OTFMI
   button = new DiagramPushButton(tr("FMI model"));
   button->setStatusTip(tr("Create a physical model defined with a FMU file"));
   connect(button, SIGNAL(clicked()), studyItem_, SLOT(createFMIModel()));
-  layout->addWidget(button, row, 0);
-  ++ row;
+  layout->addWidget(button, ++row, 0, Qt::AlignTop);
+  ++nbModels;
 #endif
+  layout->setRowStretch(row, 1);
 
-  QLabel * textEdit = new QLabel;
-  textEdit->setText(tr("Available functionalities :"));
-  textEdit->setStyleSheet("QLabel {font: bold;}");
-  layout->addWidget(textEdit, row, 0);
-
+  // functionalities list
   textEdit = new QLabel;
   QString text = QString("- %1\n- %2\n- %3\n- %4\n- %5\n- %6\n- %7")
                  .arg(tr("Evaluation"))
@@ -103,38 +122,64 @@ void StudyWindow::buildInterface()
   text.append(QString("\n- %1").arg(tr("Screening")));
 #endif
   textEdit->setText(text);
-  layout->addWidget(textEdit, row + 1, 0, Qt::AlignHCenter | Qt::AlignTop);
+  layout->addWidget(textEdit, 2, 1, nbModels, 1, Qt::AlignLeft | Qt::AlignTop);
 
-  // vertical line
+  // row - HLine
+  hLayout = new QHBoxLayout;
+  hLineFrame = new QFrame;
+  hLineFrame->setFrameShape(QFrame::HLine);
+  hLayout->addWidget(hLineFrame, 1);
+  title = new QLabel;
+  title->setText(tr("Field function"));
+  title->setStyleSheet("border: 1px solid black");
+  hLayout->addWidget(title);
 
-  QFrame * buttonsFrame = new QFrame;
-  buttonsFrame->setFrameShape(QFrame::VLine);
-  layout->addWidget(buttonsFrame, 0, 2, row + 2, 1);
+  hLineFrame = new QFrame;
+  hLineFrame->setFrameShape(QFrame::HLine);
+  hLayout->addWidget(hLineFrame, 1);
+  layout->addLayout(hLayout, ++row, 0, 1, 2);
 
-  textEdit = new QLabel;
-  textEdit->setText(tr("Available functionalities :"));
-  textEdit->setStyleSheet("QLabel {font: bold;}");
-  layout->addWidget(textEdit, row, 1);
+  // row - buttons
+  button = new DiagramPushButton(tr("Symbolic Field model"));
+  button->setStatusTip(tr("Create a physical model with outputs defined by analytical formulae"));
+  connect(button, SIGNAL(clicked()), studyItem_, SLOT(createSymbolicFieldModel()));
+  layout->addWidget(button, ++row, 0);
 
   textEdit = new QLabel;
   text = QString("- %1\n- %2")
          .arg(tr("Evaluation"))
          .arg(tr("Central tendency"));
   textEdit->setText(text);
-  layout->addWidget(textEdit, row + 1, 1, Qt::AlignHCenter | Qt::AlignTop);
+  layout->addWidget(textEdit, row, 1, Qt::AlignLeft | Qt::AlignTop);
 
-  // right side
+  button = new DiagramPushButton(tr("Python Field model"));
+  button->setStatusTip(tr("Create a physical model defined with a Python script"));
+  connect(button, SIGNAL(clicked()), studyItem_, SLOT(createPythonFieldModel()));
+  layout->addWidget(button, ++row, 0);
+
+  // row - HLine
+  hLayout = new QHBoxLayout;
+  hLineFrame = new QFrame;
+  hLineFrame->setFrameShape(QFrame::HLine);
+  hLayout->addWidget(hLineFrame, 1);
+  title = new QLabel;
+  title->setText(tr("Data sample"));
+  title->setStyleSheet("border: 1px solid black");
+  hLayout->addWidget(title);
+
+  hLineFrame = new QFrame;
+  hLineFrame->setFrameShape(QFrame::HLine);
+  hLayout->addWidget(hLineFrame, 1);
+  layout->addLayout(hLayout, ++row, 0, 1, 2);
 
   button = new DiagramPushButton(tr("Data model"));
   button->setStatusTip(tr("Import a sample to create a model"));
   connect(button, SIGNAL(clicked()), studyItem_, SLOT(createDataModel()));
-  layout->addWidget(button, 0, 3);
+  layout->addWidget(button, ++row, 0, Qt::AlignTop);
 
-  textEdit = new QLabel;
-  textEdit->setText(tr("Available functionalities :"));
-  textEdit->setStyleSheet("QLabel {font: bold;}");
-  layout->addWidget(textEdit, row, 3);
+  layout->setRowStretch(row, 1);
 
+  // functionalities list
   textEdit = new QLabel;
   text = QString("- %1\n- %2\n- %3\n- %4")
          .arg(tr("Data analysis"))
@@ -142,10 +187,8 @@ void StudyWindow::buildInterface()
          .arg(tr("Dependence inference"))
          .arg(tr("Metamodel"));
   textEdit->setText(text);
-  layout->addWidget(textEdit, row + 1, 3, Qt::AlignHCenter | Qt::AlignTop);
+  layout->addWidget(textEdit, row, 1, Qt::AlignLeft | Qt::AlignTop);
 
-  // set widget
-  mainLayout->addLayout(layout);
-  mainLayout->addStretch();
+  layout->setRowStretch(++row, 1);
 }
 }
