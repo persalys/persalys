894ef0ff6833b5aeffc25b2de5ac47504dc3ebbf
Aurélie Ladier <ladier@phimeca.com>
2019-03-29 09:45:52 +0100

    Fix MorrisResultTableModel::data: emit a signal for CheckableHeaderView

--- lib/src/model/otgui/MorrisResultTableModel.hxx
+++ lib/src/model/otgui/MorrisResultTableModel.hxx
@@ -43,7 +43,6 @@ public:
   int rowCount(const QModelIndex &parent = QModelIndex()) const;
   Qt::ItemFlags flags(const QModelIndex & index) const;
   QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-  bool setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role = Qt::EditRole);
   QVariant data(const QModelIndex &index, int role) const;
   bool setData(const QModelIndex & index, const QVariant & value, int role);
 
