059cad6f1365aa7cee0f7d25f07a81e52bd630c5
Aurélie Ladier <ladier@phimeca.com>
2019-04-09 10:46:59 +0200

    Fix #191 check collection dimension in MonteCarloResultWindow

--- lib/src/view/MonteCarloResultWindow.cxx
+++ lib/src/view/MonteCarloResultWindow.cxx
@@ -54,8 +54,10 @@ void MonteCarloResultWindow::initialize(AnalysisItem* item)
     analysisStopCriteriaMessage_ = tr("Maximum calls reached");
   if (!analysis.getWarningMessage().empty())
     analysisStopCriteriaMessage_ = tr("An error has occurred during the execution of the analysis");
-  for (UnsignedInteger i = 0; i < result_.getCoefficientOfVariation().getSize(); ++i)
-    if (result_.getCoefficientOfVariation()[i][0] <= analysis.getMaximumCoefficientOfVariation())
+  const UnsignedInteger nbInputs =  designOfExperiment_.getInputSample().getDimension();
+  for (UnsignedInteger i = nbInputs; i < result_.getCoefficientOfVariation().getSize(); ++i)
+    if (result_.getCoefficientOfVariation()[i].getSize() == 1 &&
+        result_.getCoefficientOfVariation()[i][0] <= analysis.getMaximumCoefficientOfVariation())
       analysisStopCriteriaMessage_ = tr("Maximum coefficient of variation reached");
 
   // if not one of the previous criteria
