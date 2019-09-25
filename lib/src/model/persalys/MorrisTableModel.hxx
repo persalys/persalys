d7b2c8aefa0498644f57be517799c30ccd69c263
Aurélie Ladier <ladier@phimeca.com>
2019-09-25 16:04:37 +0200

    Improve ScreeningAnalysisWizard test

--- lib/src/model/persalys/MorrisTableModel.hxx
+++ lib/src/model/persalys/MorrisTableModel.hxx
@@ -37,7 +37,7 @@ public:
   int columnCount(const QModelIndex & parent = QModelIndex()) const;
   int rowCount(const QModelIndex & parent = QModelIndex()) const;
   QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-  QVariant data(const QModelIndex & index, int role) const;
+  QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
   bool setData(const QModelIndex & index, const QVariant & value, int role);
   Qt::ItemFlags flags(const QModelIndex & index) const;
   MorrisAnalysis getAnalysis() const;
