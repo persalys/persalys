2e3e9ac28edc6876d12e8078675bead5c7724ce0
Aurélie Ladier <ladier@phimeca.com>
2019-09-30 14:20:51 +0200

    InferenceAnalysis #171. Reverse commit 95638174.

--- lib/src/base/InferenceAnalysis.cxx
+++ lib/src/base/InferenceAnalysis.cxx
@@ -197,11 +197,12 @@ void InferenceAnalysis::launch()
       DistributionFactory distFactory(distFactoriesForEachInterestVar_[sample.getDescription()[i]][j]);
       try
       {
+        Distribution distribution(distFactory.build(sample.getMarginal(i)));
+
         // Kolmogorov test
-        TestResult testResult(FittingTest::Kolmogorov(sample.getMarginal(i), distFactory, level_));
+        TestResult testResult(FittingTest::Kolmogorov(sample.getMarginal(i), distribution, level_));
 
         // BIC test
-        Distribution distribution(distFactory.build(sample.getMarginal(i)));
         const Scalar bicResult = FittingTest::BIC(sample.getMarginal(i), distribution, distribution.getParameterDimension());
 
         // set fittingTestResult
