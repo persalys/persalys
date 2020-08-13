3b7eaafcf2d320fbbd9fef6f798882e35251e1e3
Julien Schueller <schueller@phimeca.com>
2020-08-13 15:38:51 +0200

    Translation: Simplify load

--- main/main.cxx
+++ main/main.cxx
@@ -57,17 +57,11 @@ int main(int argc, char *argv[])
     QSettings().setValue("currentDir", QSettings().fileName());
 
   // translations
-  QTranslator qtTranslator;
-  qtTranslator.load("qt_" + QLocale::system().name(), ":/translations");
-  app.installTranslator(&qtTranslator);
-
-  QTranslator qtbaseTranslator;
-  qtbaseTranslator.load("qtbase_" + QLocale::system().name(), ":/translations");
-  app.installTranslator(&qtbaseTranslator);
-
-  QTranslator appTranslator;
-  appTranslator.load("persalys_" + QLocale::system().name(), ":/translations");
-  app.installTranslator(&appTranslator);
+  QTranslator translator;
+  translator.load("qt_" + QLocale::system().name(), ":/translations");
+  translator.load("qtbase_" + QLocale::system().name(), ":/translations");
+  translator.load("persalys_" + QLocale::system().name(), ":/translations");
+  app.installTranslator(&translator);
 
   // main window
   MainWindow window;
