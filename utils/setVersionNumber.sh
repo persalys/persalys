a68e9f86ce0742659a6abd679232d525b9b59855
Antoine DUMAS <dumas@phimeca.com>
2021-01-29 15:18:30 +0100

    get full end of version as patch number

--- utils/setVersionNumber.sh
+++ utils/setVersionNumber.sh
@@ -26,10 +26,10 @@ sed -i "s/ ($oldversion-/ ($version-/g" distro/debian/changelog
 
 oldmajor=`echo $oldversion | cut -d "." -f 1`
 oldminor=`echo $oldversion | cut -d "." -f 2`
-oldpatch=`echo $oldversion | cut -d "." -f 3`
+oldpatch=`echo $oldversion | cut -d "." -f 3-`
 major=`echo $version | cut -d "." -f 1`
 minor=`echo $version | cut -d "." -f 2`
-patch=`echo $version | cut -d "." -f 3`
+patch=`echo $version | cut -d "." -f 3-`
 
 sed -i "s/CPACK_PACKAGE_VERSION_MAJOR $oldmajor/CPACK_PACKAGE_VERSION_MAJOR $major/g" CMakeLists.txt
 sed -i "s/CPACK_PACKAGE_VERSION_MINOR $oldminor/CPACK_PACKAGE_VERSION_MINOR $minor/g" CMakeLists.txt
