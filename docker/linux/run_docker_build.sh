3382998cc8746713f837af724a5e198750ef8750
Julien Schueller <schueller@phimeca.com>
2021-05-04 17:13:27 +0200

    AppImage: Add mpc/gmp libs

--- docker/linux/run_docker_build.sh
+++ docker/linux/run_docker_build.sh
@@ -61,11 +61,12 @@ EOF
 cp -v /io/images/Ps-icon-32.png persalys.AppDir/persalys.png
 
 # system libs
-for libname in lapack blas quadmath xml2 gfortran ffi lzma harfbuzz mpfr
+for libname in lapack blas quadmath xml2 gfortran ffi lzma harfbuzz mpfr mpc
 do
   cp -v /usr/lib64/lib${libname}.so.[0-9] persalys.AppDir/usr/lib
 done
 cp -v /usr/lib64/libssl.so.10 /usr/lib64/libcrypto.so.10 persalys.AppDir/usr/lib
+cp -v /usr/lib64/libgmp.so.10 persalys.AppDir/usr/lib
 cp -v /usr/lib64/libtcl8.5.so /usr/lib64/libtk8.5.so persalys.AppDir/usr/lib
 cp -v /usr/lib64/atlas/lib*.so.3 persalys.AppDir/usr/lib
 cp -v /usr/lib64/libpng15.so.15 persalys.AppDir/usr/lib
