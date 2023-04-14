ba699ff8cbfad3fddc6cd7d8b8577c5bec6f12f5
Guillaume Garcia <garcia@phimeca.com>
2023-04-14 11:56:42 +0200

    removed PYTHONPATH/PYTHONHOME for Ansys coupling command

--- lib/src/base/AnsysParser.cxx
+++ lib/src/base/AnsysParser.cxx
@@ -388,6 +388,7 @@ void AnsysParser::populateCouplingStep(CouplingPhysicalModel *model,
       << " -B -F " << modelFileName_
       << " -R input" << std::to_string(indStep) << ".wbjn";
   cs.setCommand(cmd);
+  cs.setEnvironment(Description({"PYTHONPATH", "PYTHONHOME"}), Description(2, ""));
 
   OSS code;
   if(getOutputVariables().getSize())
