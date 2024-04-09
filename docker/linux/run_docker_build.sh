7b7cb9ba58f9557fe2bf2e19473e2dbf4597b452
Julien Schueller <schueller@phimeca.com>
2024-04-09 11:10:47 +0200

    docker/linux: Bump swig

--- docker/linux/run_docker_build.sh
+++ docker/linux/run_docker_build.sh
@@ -13,7 +13,7 @@ mkdir -p build && cd build
 cmake \
   -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
   -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Werror -DPERSALYS_APPIMAGE" \
-  -DSWIG_COMPILE_FLAGS="-Wno-unused-parameter -Wno-shadow" \
+  -DCMAKE_SWIG_FLAGS="-Werror" -DSWIG_COMPILE_FLAGS="-Wno-unused-parameter -Wno-shadow" \
   -DPython_EXECUTABLE=/usr/local/bin/python3 \
   -DSPHINX_FLAGS="-W -T -j8" \
   -DCMAKE_INSTALL_PREFIX=/tmp/persalys.AppDir/usr -DCMAKE_INSTALL_LIBDIR=lib \
