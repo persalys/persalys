9c396a722a5198ec9f35164739dd250269dfc362
Julien Schueller <schueller@phimeca.com>
2025-07-01 10:39:29 +0200

    launcher: Simplify env copy

--- main/launcher.cxx
+++ main/launcher.cxx
@@ -77,14 +77,8 @@ int main(int argc, char *argv[])
 
 #if BOOST_VERSION >= 108800
   std::unordered_map<std::string, std::string> env;
-  for (const auto ke : boost::process::environment::current())
-  {
-    const std::string key = std::get<0>(ke).string();
-    const std::string value = std::get<1>(ke).string();
-    env[key] = value;
-    if (verbose)
-      std::cout << key << "=" << value << std::endl;
-  }
+  for (const auto entry : boost::process::environment::current())
+    env.emplace(entry.key().string(), entry.value().string());
 #else
   boost::process::environment env = boost::this_process::environment();
 #endif
