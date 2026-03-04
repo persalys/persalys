0f170877a22bc2ad661cf343d1a2c82c5d88f823
Julien Schueller <schueller@phimeca.com>
2026-03-04 09:34:45 +0100

    utils: Fix lint.sh

--- utils/lint.sh
+++ utils/lint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+set -e
 # pip install flake8 
-# TODO: reduce max line length
 
+# TODO: reduce max line length
 PATH=~/.local/bin/:$PATH flake8 python/ --ignore=W503 --max-line-length=130
 PATH=~/.local/bin/:$PATH doc8 python/doc --ignore D000 --max-line-length=160
